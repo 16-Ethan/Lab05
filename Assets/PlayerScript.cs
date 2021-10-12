191aba632239facc890ea6a65780ab496c828ecb
16-Ethan <swiftf40.ethan@gmail.com>
2021-10-12 21:45:44 +0800

    added win scene and timer

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerScript : MonoBehaviour
 {
     public Text Score;
+    public Text TimerText;
+
     public int scoreNum;
+    public float totalcoins;
+    public float timeleft;
+    public int timeRemaining;
+
+    public float timerVaule;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +24,21 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        TimerText.text = "Timer :" + timeRemaining.ToString();
         Score.text = "Score: " + scoreNum;
+        if (scoreNum == totalcoins)
+        {
+            if(timeleft<=timerVaule)
+            {
+                SceneManager.LoadScene("Win");
+            }
+        }
+        else if(timeleft<=0)
+        {
+            SceneManager.LoadScene("Lose"); 
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -26,7 +49,7 @@ public class PlayerScript : MonoBehaviour
         }
         else if (other.gameObject.tag == "water")
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene("Lose");
         }
     }
 }
